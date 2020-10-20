b17184cb7460f1dc15e58b0d9331002b807a7496
Luca <turchet.luca@gmail.com>
2020-10-20 18:38:59 +0200

    encoder completed

--- encoder.py
+++ encoder.py
@@ -23,9 +23,8 @@ TTL_PREFIXES = """
 @prefix fx:					<https://w3id.org/aufx/ontology/1.0> .
 @prefix studio:				<http://purl.org/ontology/studio/main> .
 @prefix foaf: 				<http://xmlns.com/foaf/0.1/name> .
-@prefix rdf:				<http://www.w3.org/1999/02/22-rdf-syntax-ns> .
+@prefix rdf:				<http://www.w3.org/1999/02/22-rdf-syntax-ns> .\r\n
 """
-
 
 
 
@@ -78,9 +77,6 @@ class Encoder:
                                 )
             self.var_elk_configs.update(dict_SUSHI)
 
-
-                                
-    
     def parse_SENSEI(self):
         with open(CONFIG_SENSEI) as f:
             data = json.load(f)
@@ -97,7 +93,6 @@ class Encoder:
                                 )
             self.var_elk_configs.update(dict_SENSEI)
 
-    
     def parse_MAPPINGS(self):
         with open(CONFIG_MAPPINGS) as f:
             data = json.load(f) 
@@ -116,15 +111,80 @@ class Encoder:
                                 )
             self.var_elk_configs.update(dict_MAPPINGS)
 
-
-
-
     def encode(self):
         e.parse_SUSHI()
         e.parse_SENSEI()
         e.parse_MAPPINGS()
         with e:
             e.write_on_ttl_file(TTL_PREFIXES)
+
+            if self.var_elk_configs['t_mode'] == "stereo":
+                e.write_on_ttl_file(
+f"mx:SoftwareMixer 		mx:channel     	 					_:stereo_channel .\r\n"
+f"_:stereo_channel 		rdf:type 		 					mx:StereoChannel;\r\n"
+f"						fx:name 							\"{self.var_elk_configs['t_name']}\";\r\n"
+                )
+                if self.var_elk_configs['i_engine_bus'] == 0 & self.var_elk_configs['i_track_bus'] == 0:
+                    e.write_on_ttl_file(
+f"						mx:input							con:AnalogueInput;\r\n"                    
+                    )
+                else:
+                    #We don't handle this case now.
+                    pass
+                if self.var_elk_configs['o_engine_bus'] == 0 & self.var_elk_configs['o_track_bus'] == 0:
+                    e.write_on_ttl_file(
+f"						mx:output							con:AnalogueOutput;\r\n"
+                    ) 
+                else:
+                    #We don't handle this case now.
+                    pass    
+                if self.var_elk_configs['p_name'] == "mdaDelay":
+                    e.write_on_ttl_file(       
+f"						device:component 					_:delay_effect .\r\n"
+f"_:delay_effect 			rdf:type 							studio:Delay;\r\n"
+f" 						fx:implementation  					_:plugin .\r\n"						
+f"_:plugin 				rdf:type							fx:PlugIn;\r\n"
+f"						smi:plugin_name 					\"{self.var_elk_configs['p_name']}\";\r\n"
+f"						fx:PlugInAPI 						\"{self.var_elk_configs['p_type']}\";\r\n"                                    
+                    )
+                else:
+                    #We don't handle this case now.
+                    pass    
+                if self.var_elk_configs['m_effect_parameter'] == "Feedback":
+                    e.write_on_ttl_file(
+f"						fx:has_parameter 					_:feedback_parameter .\r\n"												
+f"_:feedback_parameter	rdf:type							fx:NumericParameter;\r\n"
+f"						fx:name 							\"{self.var_elk_configs['m_effect_parameter']}\".\r\n"  						
+                    )
+                else:
+                    #We don't handle this case now.
+                    pass    
+                if self.var_elk_configs['s_id'] == self.var_elk_configs['m_mapping_id'] == 1:
+                    if self.var_elk_configs['s_name'] == "pressure_0":
+                        e.write_on_ttl_file(    
+f"smi:PressureSensor      sosa:observes 			            \"pressure\";\r\n"
+f"						foaf:name 				            \"{self.var_elk_configs['s_name']}\";\r\n"
+f"						smi:mapping_function	            _:sensor_to_parameter .\r\n"
+
+f"_:sensor_to_parameter	rdf:type 						    smi:MappingTransform;\r\n"
+f"						smi:mapping_function_type 		    \"linear\" ;\r\n"
+f"						smi:maps 						    _:feedback_parameter .\r\n"	
+                    )
+                    else:
+                        #We don't handle this case now.
+                        pass
+                else:
+                        #We don't handle this case now.
+                        pass	    					                           
+            else:
+                #This is the case of mono. Let's ignore it for the moment
+                pass
+
+
+
+
+
+
 
 
 
@@ -137,8 +197,8 @@ if __name__ == '__main__':
     e.encode()
 
 
-    for k, v in e.var_elk_configs.items():
-        print(k, v)
+   # for k, v in e.var_elk_configs.items():
+   #    print(k, v)
 
 
 
